8ba449e23303e7951f648782ad54a0b749430a37
Robert Ayettey <rayettey72@gmail.com>
2024-08-23 20:51:49 +0000

    Add SQL script to print full description of the books table

---
+++ task_4.sql
@@ -0,0 +1,16 @@
+-- task_4.sql
+
+USE alx_book_store;
+
+SELECT
+    COLUMN_NAME AS 'Column Name',
+    COLUMN_TYPE AS 'Column Type',
+    IS_NULLABLE AS 'Is Nullable',
+    COLUMN_DEFAULT AS 'Default Value',
+    EXTRA AS 'Extra Info'
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+WHERE
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'books';
+
